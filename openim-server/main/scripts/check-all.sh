7f9f9cf05a354d36f6b48540a36d152b441e6ed6
Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>
2023-09-02 14:10:12 +0800

    feat: set config file path
    Signed-off-by: Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>

--- openim-server/main/scripts/check-all.sh
+++ openim-server/main/scripts/check-all.sh
@@ -62,11 +62,11 @@ echo "+++ The port being checked: ${OPENIM_DEPENDENCY_PORT_LISTARIES[@]}"
 
 set +e
 
-if grep -qE 'docker|kubepods' /proc/1/cgroup; then
+if grep -qE 'docker|kubepods' /proc/1/cgroup || [ -f /.dockerenv ]; then
     openim::color::echo ${COLOR_BLUE} "Environment in the interior of the container"
 else
     openim::color::echo ${COLOR_BLUE} "The environment is outside the container"
-    openim::util::check_ports ${OPENIM_DEPENDENCY_PORT_LISTARIES[@]}
+    openim::util::check_ports ${OPENIM_DEPENDENCY_PORT_LISTARIES[@]} || return 0
 fi
 
 if [[ $? -ne 0 ]]; then
